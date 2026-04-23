66f00de99655165c49f58ba646f51b89f3aa239f
Leonard-Chen <leonard_reonado@qq.com>
2026-04-23 15:23:39 +0800

    修正bug：dto没有定义构造器引发错误

--- sms-common/src/main/java/com/gdut/sms/common/dto/EmployeeDTO.java
+++ sms-common/src/main/java/com/gdut/sms/common/dto/EmployeeDTO.java
@@ -1,12 +1,16 @@
 package com.gdut.sms.common.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import com.gdut.sms.common.entity.Employee;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
     private String employeeNo;
     private String employeeName;
