3cd0db6e1f77cc606310129b9722e76ebff58e0d
Leonard-Chen <leonard_reonado@qq.com>
2026-04-24 18:21:19 +0800

    新增创建和修改用户功能

--- sms-system/src/main/java/com/gdut/sms/system/service/UserService.java
+++ sms-system/src/main/java/com/gdut/sms/system/service/UserService.java
@@ -3,9 +3,12 @@ package com.gdut.sms.system.service;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import com.gdut.sms.common.entity.User;
+import com.gdut.sms.common.entity.Role;
 import com.gdut.sms.common.dto.UserDTO;
+import com.gdut.sms.common.entity.Department;
 import org.springframework.stereotype.Service;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.cache.annotation.CachePut;
 import com.gdut.sms.system.repository.UserRepository;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.CacheEvict;
@@ -52,6 +55,58 @@ public class UserService implements UserDetailsService {
         return userRepository.findAll().stream()
                 .map(UserDTO::new)
                 .toList();
+    }
+
+    @Transactional
+    @Caching(
+            evict = @CacheEvict(value = "user_list", allEntries = true)
+    )
+    public UserDTO create(UserDTO dto) throws RuntimeException {
+        if (userRepository.findByUsername(dto.getUsername()).isPresent()) {
+            throw new RuntimeException("用户已存在");
+        }
+
+        return save(dto);
+    }
+
+    @Transactional
+    @Caching(
+            put = @CachePut(value = "user", key = "#dto.username"),
+            evict = @CacheEvict(value = "user_list", allEntries = true)
+    )
+    public UserDTO update(UserDTO dto) throws RuntimeException {
+        return save(dto);
+    }
+
+    public UserDTO save(UserDTO dto) throws RuntimeException {
+        if (dto.getUsername() == null || dto.getUsername().isEmpty()
+                || dto.getPassword() == null || dto.getPassword().isEmpty()) {
+            throw new IllegalArgumentException("用户名和密码不能为空");
+        }
+
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setRealName(dto.getRealName());
+        user.setPassword(dto.getPassword());
+        user.setStatus(dto.getStatus());
+        user.setEmail(dto.getEmail());
+        user.setPhone(dto.getPhone());
+
+        Department dept = null;
+        if (dto.getDeptNo() != null && !dto.getDeptNo().isEmpty()) {
+            dept = new Department();
+            dept.setDeptNo(dto.getDeptNo());
+        }
+        user.setDept(dept);
+
+        Role role = null;
+        if (dto.getRoleCode() != null && !dto.getRoleCode().isEmpty()) {
+            role = new Role();
+            role.setRoleCode(dto.getRoleCode());
+        }
+        user.setRole(role);
+
+        return new UserDTO(userRepository.save(user));
     }
 
     @Transactional
